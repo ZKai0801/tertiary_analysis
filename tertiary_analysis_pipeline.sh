3be6b04a3c022e32611f43c4b353dbbb5dc4baf8
Zhentian Kai <38963539+ZKai0801@users.noreply.github.com>
2020-04-13 16:21:17 +0800

    flip the order of step2 and step3

--- tertiary_analysis_pipeline.sh
+++ tertiary_analysis_pipeline.sh
@@ -24,17 +24,20 @@ anno_civic=""
 echo "** Step1: filtering starts at `date`"
 grep "PASS\|#" $input_vcf > ${input_vcf%%.vcf*}.step1_filtered.vcf
 
-# step2: normalisation of vcf file
-echo "** Step2: normalisation starts at `date`"
-bcftools norm -m -both -f ${reference} ${input_vcf%%.vcf*}.step1_filtered.vcf -o ${input_vcf%%.vcf*}.step2_normalised.vcf
 
-# step3: merge multi-nucleotide polymorphism
-echo "** Step3: merging starts at `date`"
-python3 $merge_mnv ${input_vcf%%.vcf*}.step2_normalised.vcf ${reference} -o ${input_vcf%%.vcf*}.step3_merged.vcf
+# step2: merge multi-nucleotide polymorphism
+echo "** Step2: merging starts at `date`"
+python3 $merge_mnv ${input_vcf%%.vcf*}.step1_filtered.vcf ${reference} -o ${input_vcf%%.vcf*}.step2_merged.vcf
+
+
+# step3: normalisation of vcf file
+echo "** Step3: normalisation starts at `date`"
+bcftools norm -m -both -f ${reference} ${input_vcf%%.vcf*}.step2_merged.vcf -o ${input_vcf%%.vcf*}.step3_normalised.vcf
+
 
 # step4: annotate population freq, software prediction results etc.
-echo "** Step3: annotation starts at `date`"
-table_annovar.pl ${input_vcf%%.vcf*}.step3_merged.vcf ${annovar_dir} \
+echo "** Step4 annotation starts at `date`"
+table_annovar.pl ${input_vcf%%.vcf*}.step3_normalised.vcf ${annovar_dir} \
 --outfile ${input_vcf%%.vcf*}.step4_temp --buildver hg19  \
 --protocol 1000g2015aug_all,esp6500siv2_all,exac03nonpsych,cosmic70,clinvar_20191013,intervar_20180118,dbnsfp35a,avsnp150,dbscsnv11  \
 --operation f,f,f,f,f,f,f,f,f  --vcfinput --remove
@@ -47,8 +50,10 @@ vep --dir ${vep_dir} --cache --offline --cache_version 98 \
 
 rm ${input_vcf%%.vcf*}.step4_temp.hg19_multianno.vcf ${input_vcf%%.vcf*}.step4_temp.hg19_multianno.txt
 
+
 # step5: select for correct transcript
 python3 $anno_hgvs ${input_vcf%%.vcf*}.step4_anno.vcf ${clinic_transcripts} ${refFlat} -o ${input_vcf%%.vcf*}.step5_hgvs.vcf
+
 
 # step6: annotate clinical significant associated with each variant
 python3 $anno_civic ${input_vcf%%.vcf*}.step5_hgvs.vcf ${civic} -o ${input_vcf%%.vcf*}.step6_civic.vcf
